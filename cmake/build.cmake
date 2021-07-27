d49036801eee4c5d737b845b19d8fe870d46fe28
Antonino Natale <antonio.natale97@hotmail.com>
2021-07-27 15:02:12 +0200

    Update dist flags

--- cmake/build.cmake
+++ cmake/build.cmake
@@ -4,7 +4,7 @@ set(CMAKE_CXX_STANDARD_REQUIRED True)
 if(CMAKE_CXX_COMPILER_ID MATCHES GNU OR CMAKE_CXX_COMPILER_ID MATCHES Clang)
     set(CMAKE_CXX_FLAGS                     "${CMAKE_CXX_FLAGS} -pipe")
     set(CMAKE_CXX_FLAGS_DEBUG               "${CMAKE_CXX_FLAGS_DEBUG} -O0 -g3 -ggdb -Wall -Werror")
-    set(CMAKE_CXX_FLAGS_RELEASE             "${CMAKE_CXX_FLAGS_RELEASE} -O3 -march=native -fomit-frame-pointer -fno-plt -flto -D_FORTIFY_SOURCE=2 -DNDEBUG=1")
+    set(CMAKE_CXX_FLAGS_RELEASE             "${CMAKE_CXX_FLAGS_RELEASE} -O3 -fomit-frame-pointer -fno-plt -flto -D_FORTIFY_SOURCE=2 -DNDEBUG=1")
 
     if(ENABLE_COVERAGE)
         set(CMAKE_CXX_FLAGS_DEBUG           "${CMAKE_CXX_FLAGS_DEBUG} -fprofile-arcs -ftest-coverage --coverage")
@@ -26,6 +26,16 @@ if(CMAKE_CXX_COMPILER_ID MATCHES Clang)
     set(CMAKE_EXE_LINKER_FLAGS              "${CMAKE_EXE_LINKER_FLAGS} -lstdc++ -lc -lm")
 endif()
 
+
+if(ENABLE_STATIC)
+    set(CMAKE_EXE_LINKER_FLAGS              "${CMAKE_EXE_LINKER_FLAGS} -static")
+endif()
+
+if(ENABLE_NATIVE)
+    set(CMAKE_CXX_FLAGS_RELEASE             "${CMAKE_CXX_FLAGS_RELEASE} -march=native")
+else()
+    set(CMAKE_CXX_FLAGS_RELEASE             "${CMAKE_CXX_FLAGS_RELEASE} -march=x86-64 -mtune=generic")
+endif()
 
 
 if(NOT CMAKE_BUILD_TYPE)
